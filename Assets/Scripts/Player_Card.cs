7570e30788850bd77eb166e8f370c75a962e13ca
SatoShinta <kanten3005@gmail.com>
2024-08-25 18:00:43 +0900

    カードを表示する場所を追加

---
+++ Assets/Scripts/Player_Card.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Card : MonoBehaviour
+{
+    [SerializeField, Header("カードの位置")] GameObject[] _playerCardPos;
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
