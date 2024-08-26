0f58b3f81984c87caa8504562907d3285aee4530
SatoShinta <kanten3005@gmail.com>
2024-08-26 21:10:14 +0900

    コメントを入力

--- Assets/Scripts/Player_Card.cs
+++ Assets/Scripts/Player_Card.cs
@@ -6,20 +6,24 @@ public class Player_Card : MonoBehaviour
     [SerializeField, Header("カードの位置")] private GameObject[] _playerCardPos;
     [SerializeField] private GameObject _playerCard;
 
-    [SerializeField] private float _spawnInterval = 0.5f;
-    // int _spawnCount = 3;
+    [SerializeField,Header("カードの発生間隔")] private float _spawnInterval = 0.5f;
+    
 
     IEnumerator SpawnCoroutine()
     {
+        //_playerCardPosに格納されているオブジェクトがあるだけ実行する
         foreach (var pos in _playerCardPos)
         {
+            //_spawnIntervalの時間分だけ実行を待つ
             yield return new WaitForSeconds(_spawnInterval);
+            //_playerCardに格納されているカードを_playerCardPosの場所に生成する
             Instantiate(_playerCard, pos.transform.position, Quaternion.identity);
         }
     }
 
     private void Start()
     {
+        //ゲーム開始時に実行
         StartCoroutine(SpawnCoroutine());
     }
 }
--- Assets/Scripts/Slot1.cs
+++ Assets/Scripts/Slot1.cs
@@ -6,16 +6,19 @@ public class Slot1 : MonoBehaviour
 {
     public void CangeColor()
     {
+        //ランダムな色の値を決定する
         float r = Random.Range(0f, 1f);
         float g = Random.Range(0f, 1f);
         float b = Random.Range(0f, 1f);
 
+        //Colorに先ほど生成した数字を代入する
         Color randomColer = new Color(r, g, b);
 
         Renderer renderer =GetComponent<Renderer>();
 
         if(renderer != null)
         {
+            //このオブジェクトのカラーをランダムな色にする
             renderer.material.color = randomColer;
         }
         else
