9296b47961120dd5543558329f9e19dc3ab41045
SatoShinta <kanten3005@gmail.com>
2024-08-31 21:16:12 +0900

    現在自分の手札にあるカードの絵柄を取得することができるようになった

--- Assets/Scripts/Player_Card.cs
+++ Assets/Scripts/Player_Card.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Player_Card : MonoBehaviour
@@ -6,6 +7,7 @@ public class Player_Card : MonoBehaviour
     [SerializeField, Header("カードの位置")] private GameObject[] _playerCardPos;
     [SerializeField] private GameObject _playerCard;
     [SerializeField, Header("カードの絵柄")] private Sprite[] _playerCardSprite;
+    [SerializeField, Header("現在の絵柄")] public List<Sprite> _playerCardSpriteNow = new List<Sprite>();
     [SerializeField,Header("カードの発生間隔")] private float _spawnInterval = 0.5f;
 
     SpriteRenderer spriteRenderer;
@@ -23,6 +25,10 @@ public class Player_Card : MonoBehaviour
             //手札に来る絵柄をランダムにする
             int randomIndex = Random.Range(0, _playerCardSprite.Length);
             spriteRenderer.sprite = _playerCardSprite[randomIndex];
+
+            //現在の絵柄を入手する
+            _playerCardSpriteNow.Add(_playerCardSprite[randomIndex]);
+            
 
             //_playerCardに格納されているカードを_playerCardPosの場所に生成する
             Instantiate(_playerCard, pos.transform.position, Quaternion.identity);
