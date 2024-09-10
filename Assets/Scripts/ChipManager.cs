ae838ebf0e90417fdcde4e72a821ab08d68c23ea
SatoShinta <kanten3005@gmail.com>
2024-09-10 18:50:36 +0900

    チップを賭けられるようになった

--- Assets/Scripts/ChipManager.cs
+++ Assets/Scripts/ChipManager.cs
@@ -1,21 +1,31 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class ChipManager : MonoBehaviour
 {
-    [SerializeField, Header("プレイヤーのチップ")] int _PlayerChip = 500;
-    [SerializeField, Header("敵のチップ")] int _EnemyChip = 500;
+    [SerializeField, Header("プレイヤーのチップ")] int _maxPlayerChip;
+    [SerializeField, Header("敵のチップ")] int _maxEnemyChip;
+    int pTotalAmount;
+    int eTotalAmount;
     [SerializeField] GameObject _Player;
     [SerializeField] GameObject _Enemy;
-    Slider Slider;
-    InputField InputField;
+
+    [SerializeField] Slider pSlider;
+    [SerializeField] Slider eSlider;
+    [SerializeField] InputField pInputField;
+    [SerializeField] InputField eInputField;
+    [SerializeField] Text PlayerChipText;
+    [SerializeField] Text EnemyChipText;
+    public bool _betChips;
+    public bool _gameStart;
 
     public void Start()
     {
-        Slider = GetComponent<Slider>();
-        InputField = GetComponent<InputField>();
+        //pSlider = GetComponent<Slider>();
+        //pInputField = GetComponent<InputField>();
+        _maxPlayerChip = 500;
+        _maxEnemyChip = 500;
+        _betChips = true;
     }
 
     public void Update()
@@ -23,5 +33,49 @@ public class ChipManager : MonoBehaviour
         
     }
 
+    public void PlayerChipsDecision()
+    {
+        
+        int betAmount;
+        if (int.TryParse(pInputField.text, out betAmount) && betAmount >= 0)
+        {
+            pTotalAmount += betAmount;
+            _maxPlayerChip -= betAmount;
+            PlayerChipText.text = "賭けたチップ\n" +  pTotalAmount.ToString();
+            PUpdateUI();
+        }
+        else
+        {
+            Debug.Log("aaa");
+        }
 
+    }
+
+    public void EnemyChipsDecision()
+    {
+        int betAmount;
+        if (int.TryParse(eInputField.text, out betAmount) && betAmount >= 0)
+        {
+            eTotalAmount += betAmount;
+            _maxEnemyChip -= betAmount;
+            EnemyChipText.text = "賭けたチップ\n" + eTotalAmount.ToString();
+            EUpdateUI();
+        }
+        else
+        {
+            Debug.Log("bbb");
+        }
+    }
+
+    public void PUpdateUI()
+    {
+        pSlider.value = _maxPlayerChip;
+        pInputField.text = string.Empty;
+    }
+
+    public void EUpdateUI()
+    {
+        eSlider.value = _maxEnemyChip;
+        eInputField.text = string.Empty;
+    }
 }
